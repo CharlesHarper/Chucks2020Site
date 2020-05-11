1f3111dd0cddf6d6447835db50eded615ba997be
Chuck <39815619+CharlesHarper@users.noreply.github.com>
2020-05-11 19:32:06 -0400

    more work on about me

--- src/Components /About.jsx
+++ src/Components /About.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import {
   MDBMask,
   MDBRow,
@@ -10,168 +10,197 @@ import {
   MDBCard,
   MDBCardBody,
   MDBCollapse,
-  MDBIcon
-
-
-  
-} from 'mdbreact';
-import "../Styles/ParallaxIntro.scss"
-import mepic from "../Images/me.jpeg"
-import NavChuck from "../Components /NavChuck.jsx"
-
-
+  MDBIcon,
+  MDBCardTitle,
+  MDBAnimation
+} from "mdbreact";
+import "../Styles/ParallaxIntro.scss";
+import mepic from "../Images/me.jpeg";
+import NavChuck from "../Components /NavChuck.jsx";
 
 class About extends React.Component {
   state = {
     collapseID: "collapse0"
   };
   toggleCollapse = collapseID => () =>
-  this.setState(prevState => ({
-    collapseID: prevState.collapseID !== collapseID ? collapseID : ""
-  }));
+    this.setState(prevState => ({
+      collapseID: prevState.collapseID !== collapseID ? collapseID : ""
+    }));
 
-render() {
+  render() {
     return (
-     
-      <div >
-       <MDBView
-          src={mepic}
-          fixed
-          
-        >
-          <MDBMask className='rgba-white-light' />
+      <div>
+        <MDBView src={mepic} fixed>
+          <MDBMask className="rgba-white-light" />
 
-          <NavChuck/>
+          <NavChuck />
 
-          
           <Router>
-          <MDBContainer
-            className='d-flex justify-content-center align-items-center'
-            style={{ paddingTop: '5%' }}
-          >
-             <MDBCard
-          className="card-image"
-        
-        >
-          <div className="py-5 px-4"
-       
-          >
-            <MDBRow className="d-flex justify-content-center" >
-              <MDBCol md="10" xl="8">
-                <MDBContainer className="accordion md-accordion accordion-5">
-                  <MDBCard className="mb-4">
-                    <MDBCollapseHeader
-                      onClick={this.toggleCollapse("collapse1")}
-                      className="p-0 z-depth-1"
-                      tag="h4"
-                      tagClassName="text-uppercase white-text mb-0 d-flex justify-content-start align-items-center"
-                    >
-                      <div
-                        className="d-flex justify-content-center align-items-center mr-4"
-                        style={{ backgroundColor: "#fff", minWidth: "100px" }}
-                      >
-                        <MDBIcon
-                          icon="cloud"
-                          size="2x"
-                          className="m-3 black-text "
-                        />
-                      </div>
-                      Past 
-                    </MDBCollapseHeader>
+          <MDBAnimation
+              reveal
+              type="bounceInUp"
+            
+              duration="10s"
+              >
+          <h1
+                className="d-flex justify-content-center align-items-center"
+                style={{ paddingTop: "4%", color: "blue", fontWeight: "700" }}
+              >
+                About Me
+              </h1>
+              </MDBAnimation>
+            <MDBAnimation
+              reveal
+              type="bounceInUp"
+              duration="7000ms"
+              delay=".5s"
+              className="pb-3"
+            >
+              
+              <MDBContainer
+                className="d-flex justify-content-center align-items-center"
+                style={{ paddingTop: "1%" }}
+              >
+                <MDBCard className="card-image">
+                  <div className="py-5 px-4">
+                    <MDBRow className="d-flex justify-content-center">
+                      <MDBCol md="10" xl="8">
+                        <MDBContainer className="accordion md-accordion accordion-5">
+                          <MDBCard className="mb-4">
+                            <MDBCollapseHeader
+                              onClick={this.toggleCollapse("collapse1")}
+                              className="p-0 z-depth-1"
+                              tag="h4"
+                              tagClassName="text-uppercase white-text mb-0 d-flex justify-content-start align-items-center"
+                            >
+                              <div
+                                className="d-flex justify-content-center align-items-center mr-4"
+                                style={{
+                                  backgroundColor: "#fff",
+                                  minWidth: "100px"
+                                }}
+                              >
+                                <MDBIcon
+                                  icon="cloud"
+                                  size="2x"
+                                  className="m-3 black-text "
+                                />
+                              </div>
+                              Past
+                            </MDBCollapseHeader>
 
-                    <MDBCollapse id="collapse1" isOpen={this.state.collapseID}>
-                      <MDBCardBody className="rgba-black-light white-text z-depth-1">
-                        <p className="p-md-4 mb-0">
-                          Anim pariatur cliche reprehenderit, enim eiusmod high
-                          life accusamus terry richardson ad squid. 3 wolf moon
-                          officia aute, non cupidatat skateboard dolor brunch.
-                          Food truck quinoa nesciunt laborum eiusmod. Brunch 3
-                          wolf moon tempor, sunt aliqua put a bird on it squid
-                          single-origin coffee nulla assumenda shoreditch et.
-                        </p>
-                      </MDBCardBody>
-                    </MDBCollapse>
-                  </MDBCard>
+                            <MDBCollapse
+                              id="collapse1"
+                              isOpen={this.state.collapseID}
+                            >
+                              <MDBCardBody className="rgba-black-light white-text z-depth-1">
+                                <p className="p-md-4 mb-0">
+                                  Anim pariatur cliche reprehenderit, enim
+                                  eiusmod high life accusamus terry richardson
+                                  ad squid. 3 wolf moon officia aute, non
+                                  cupidatat skateboard dolor brunch. Food truck
+                                  quinoa nesciunt laborum eiusmod. Brunch 3 wolf
+                                  moon tempor, sunt aliqua put a bird on it
+                                  squid single-origin coffee nulla assumenda
+                                  shoreditch et.
+                                </p>
+                              </MDBCardBody>
+                            </MDBCollapse>
+                          </MDBCard>
 
-                  <MDBCard className="mb-4">
-                    <MDBCollapseHeader
-                      onClick={this.toggleCollapse("collapse2")}
-                      className="p-0 z-depth-1"
-                      tag="h4"
-                      tagClassName="text-uppercase white-text mb-0 d-flex justify-content-start align-items-center"
-                    >
-                    <div
-                        className="d-flex justify-content-center align-items-center mr-4"
-                        style={{ backgroundColor: "#fff", minWidth: "100px" }}
-                      >
-                      <MDBIcon
-                        icon="comment-alt"
-                        size="2x"
-                        className="m-3 black-text"
-                      />
-                      </div>
-                      Present
-                    </MDBCollapseHeader>
+                          <MDBCard className="mb-4">
+                            <MDBCollapseHeader
+                              onClick={this.toggleCollapse("collapse2")}
+                              className="p-0 z-depth-1"
+                              tag="h4"
+                              tagClassName="text-uppercase white-text mb-0 d-flex justify-content-start align-items-center"
+                            >
+                              <div
+                                className="d-flex justify-content-center align-items-center mr-4"
+                                style={{
+                                  backgroundColor: "#fff",
+                                  minWidth: "100px"
+                                }}
+                              >
+                                <MDBIcon
+                                  icon="comment-alt"
+                                  size="2x"
+                                  className="m-3 black-text"
+                                />
+                              </div>
+                              Present
+                            </MDBCollapseHeader>
 
-                    <MDBCollapse id="collapse2" isOpen={this.state.collapseID}>
-                      <MDBCardBody className="rgba-black-light white-text z-depth-1">
-                        <p className="p-md-4 mb-0">
-                          Anim pariatur cliche reprehenderit, enim eiusmod high
-                          life accusamus terry richardson ad squid. 3 wolf moon
-                          officia aute, non cupidatat skateboard dolor brunch.
-                          Food truck quinoa nesciunt laborum eiusmod. Brunch 3
-                          wolf moon tempor, sunt aliqua put a bird on it squid
-                          single-origin coffee nulla assumenda shoreditch et.
-                        </p>
-                      </MDBCardBody>
-                    </MDBCollapse>
-                  </MDBCard>
+                            <MDBCollapse
+                              id="collapse2"
+                              isOpen={this.state.collapseID}
+                            >
+                              <MDBCardBody className="rgba-black-light white-text z-depth-1">
+                                <p className="p-md-4 mb-0">
+                                  Anim pariatur cliche reprehenderit, enim
+                                  eiusmod high life accusamus terry richardson
+                                  ad squid. 3 wolf moon officia aute, non
+                                  cupidatat skateboard dolor brunch. Food truck
+                                  quinoa nesciunt laborum eiusmod. Brunch 3 wolf
+                                  moon tempor, sunt aliqua put a bird on it
+                                  squid single-origin coffee nulla assumenda
+                                  shoreditch et.
+                                </p>
+                              </MDBCardBody>
+                            </MDBCollapse>
+                          </MDBCard>
 
-                  <MDBCard className="mb-4">
-                    <MDBCollapseHeader
-                      onClick={this.toggleCollapse("collapse3")}
-                      className="p-0 z-depth-1"
-                      tag="h4"
-                      tagClassName="text-uppercase white-text mb-0 d-flex justify-content-start align-items-center"
-                    >
-                    <div
-                        className="d-flex justify-content-center align-items-center mr-4"
-                        style={{ backgroundColor: "#fff", minWidth: "100px" }}
-                      >
-                      <MDBIcon
-                        icon="cogs"
-                        size="2x"
-                        className="m-3 black-text"
-                      />
-                      </div>
-                      Future 
-                    </MDBCollapseHeader>
+                          <MDBCard className="mb-4">
+                            <MDBCollapseHeader
+                              onClick={this.toggleCollapse("collapse3")}
+                              className="p-0 z-depth-1"
+                              tag="h4"
+                              tagClassName="text-uppercase white-text mb-0 d-flex justify-content-start align-items-center"
+                            >
+                              <div
+                                className="d-flex justify-content-center align-items-center mr-4"
+                                style={{
+                                  backgroundColor: "#fff",
+                                  minWidth: "100px"
+                                }}
+                              >
+                                <MDBIcon
+                                  icon="cogs"
+                                  size="2x"
+                                  className="m-3 black-text"
+                                />
+                              </div>
+                              Future
+                            </MDBCollapseHeader>
 
-                    <MDBCollapse id="collapse3" isOpen={this.state.collapseID}>
-                      <MDBCardBody className="rgba-black-light white-text z-depth-1">
-                        <p className="p-md-4 mb-0">
-                          Anim pariatur cliche reprehenderit, enim eiusmod high
-                          life accusamus terry richardson ad squid. 3 wolf moon
-                          officia aute, non cupidatat skateboard dolor brunch.
-                          Food truck quinoa nesciunt laborum eiusmod. Brunch 3
-                          wolf moon tempor, sunt aliqua put a bird on it squid
-                          single-origin coffee nulla assumenda shoreditch et.
-                        </p>
-                      </MDBCardBody>
-                    </MDBCollapse>
-                  </MDBCard>
-                </MDBContainer>
-              </MDBCol>
-            </MDBRow>
-          </div>
-        </MDBCard>
-           
-          </MDBContainer>
+                            <MDBCollapse
+                              id="collapse3"
+                              isOpen={this.state.collapseID}
+                            >
+                              <MDBCardBody className="rgba-black-light white-text z-depth-1">
+                                <p className="p-md-4 mb-0">
+                                  Anim pariatur cliche reprehenderit, enim
+                                  eiusmod high life accusamus terry richardson
+                                  ad squid. 3 wolf moon officia aute, non
+                                  cupidatat skateboard dolor brunch. Food truck
+                                  quinoa nesciunt laborum eiusmod. Brunch 3 wolf
+                                  moon tempor, sunt aliqua put a bird on it
+                                  squid single-origin coffee nulla assumenda
+                                  shoreditch et.
+                                </p>
+                              </MDBCardBody>
+                            </MDBCollapse>
+                          </MDBCard>
+                        </MDBContainer>
+                      </MDBCol>
+                    </MDBRow>
+                  </div>
+                </MDBCard>
+              </MDBContainer>
+            </MDBAnimation>
           </Router>
         </MDBView>
-      
       </div>
-      
     );
   }
 }
--- src/Components /NavChuck.jsx
+++ src/Components /NavChuck.jsx
@@ -1,68 +1,64 @@
-import React, { Component } from 'react';
-import { MDBNavbar, MDBNav, MDBNavItem, MDBNavLink,  MDBContainer } from 'mdbreact';
+import React, { Component } from "react";
+import {
+  MDBNavbar,
+  MDBNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBContainer
+} from "mdbreact";
 
 class NavChuck extends Component {
-state = {
-  collapse1: false,
-  collapseID: ''
-}
-
-toggleCollapse = collapseID => () => {
-  this.setState(prevState => ({ collapseID: (prevState.collapseID !== collapseID ? collapseID : '') }));
-}
-
-toggleSingleCollapse = collapseId => {
-  this.setState({
-    ...this.state,
-    [collapseId]: !this.state[collapseId]
-  });
-}
-
-
-render() {
-  
-  
-  const navStyle = {
-    color: "white",
-    backgroundColor: "rgb(255,255,255, 0.4)",
-    padding: "10px",
-    fontFamily: "Arial",
-    marginTop: "10px",
-    fontSize: "1.2em",
-    
+  state = {
+    collapse1: false,
+    collapseID: ""
   };
 
-  return (
-  
-      <MDBContainer >
-        <MDBNavbar  style={navStyle} className="justify-content-center" >
-          
-         
-           <MDBNav className=" flex-column flex-sm-row ">
-<MDBNavItem>
-  <MDBNavLink active to="/">Home</MDBNavLink>
-</MDBNavItem>
-<MDBNavItem>
-  <MDBNavLink to={'/AboutView'}>About Me</MDBNavLink>
-</MDBNavItem>
-<MDBNavItem>
-  <MDBNavLink to={'/ContactView'}>Contact Me</MDBNavLink>
-</MDBNavItem>
-<MDBNavItem>
-  <MDBNavLink to={'/ProjectsView'}>Projects</MDBNavLink>
-</MDBNavItem>
-</MDBNav>
-        
+  toggleCollapse = collapseID => () => {
+    this.setState(prevState => ({
+      collapseID: prevState.collapseID !== collapseID ? collapseID : ""
+    }));
+  };
+
+  toggleSingleCollapse = collapseId => {
+    this.setState({
+      ...this.state,
+      [collapseId]: !this.state[collapseId]
+    });
+  };
+
+  render() {
+    const navStyle = {
+      color: "white",
+      backgroundColor: "rgb(255,255,255, 0.4)",
+      padding: "10px",
+      fontFamily: "Arial",
+      marginTop: "10px",
+      fontSize: "1.2em"
+    };
+
+    return (
+      <MDBContainer>
+        <MDBNavbar style={navStyle} className="justify-content-center">
+          <MDBNav className=" flex-column flex-sm-row ">
+            <MDBNavItem>
+              <MDBNavLink active to="/">
+                Home
+              </MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink to={"/AboutView"}>About Me</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink to={"/ContactView"}>Contact Me</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink to={"/ProjectsView"}>Projects</MDBNavLink>
+            </MDBNavItem>
+          </MDBNav>
         </MDBNavbar>
       </MDBContainer>
-
-
-  
     );
   }
 }
 
 export default NavChuck;
-
-
-
