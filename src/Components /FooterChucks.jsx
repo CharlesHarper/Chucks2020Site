0bc0e82e77156d696f5396d6ff43e97877023121
Chuck <39815619+CharlesHarper@users.noreply.github.com>
2020-05-11 20:07:13 -0400

    add social to footer

--- src/Components /FooterChucks.jsx
+++ src/Components /FooterChucks.jsx
@@ -8,29 +8,41 @@ const FooterChucks = () => {
         <MDBRow>
           <MDBCol md="12" className="py-5">
             <div className="mb-5 flex-center">
-              <a className="fb-ic">
-                <i className="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x">
-                </i>
+              <a
+                href="https://www.facebook.com/Charles.Harper.Miami"
+                target="_blank"
+                className="fb-ic"
+              >
+                <i className="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
               </a>
-              <a className="tw-ic">
-                <i className="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x">
-                </i>
+              <a
+                href="https://twitter.com/chuck89286795"
+                target="_blank"
+                className="tw-ic"
+              >
+                <i className="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
               </a>
-              <a className="gplus-ic">
+              {/* <a href="https://www.facebook.com/Charles.Harper.Miami" target="_blank"  className="gplus-ic">
                 <i className="fab fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x">
                 </i>
+              </a> */}
+              <a
+                href="https://www.linkedin.com/in/charles-harper-ghrs/"
+                target="_blank"
+                className="li-ic"
+              >
+                <i className="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
               </a>
-              <a className="li-ic">
-                <i className="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x">
-                </i>
+              <a
+                href="https://www.instagram.com/charleshrp102992"
+                target="_blank"
+                className="ins-ic"
+              >
+                <i className="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"></i>
               </a>
-              <a className="ins-ic">
-                <i className="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x">
-                </i>
-              </a>
-              <a className="pin-ic">
+              {/* <a href="https://www.facebook.com/Charles.Harper.Miami"  target="_blank"className="pin-ic">
                 <i className="fab fa-pinterest fa-lg white-text fa-2x"> </i>
-              </a>
+              </a> */}
             </div>
           </MDBCol>
         </MDBRow>
@@ -38,11 +50,10 @@ const FooterChucks = () => {
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
           &copy; {new Date().getFullYear()} Copyright:{" Charles Harper"}
-          
         </MDBContainer>
       </div>
     </MDBFooter>
   );
-}
+};
 
 export default FooterChucks;
